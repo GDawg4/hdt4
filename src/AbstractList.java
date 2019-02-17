4de4e6776dc0654c09a687f6beb62714908b1245
eli17048 <42878138+eli17048@users.noreply.github.com>
2019-02-16 20:38:17 -0600

    Add files via upload
    Stack y Lista Abstract

---
+++ src/AbstractList.java
@@ -0,0 +1,33 @@
+/**
+ * Clase abstracta que contiene dos metodos comunes en las implementaciones de listas y que <p>
+ * implementa a la interfaz iLista.
+ * @author Andrea Elias
+ * Rodrifo Garoz
+ */
+public abstract class AbstractList<E> implements iLista<E>{
+    /**
+     * Constructor vacio.
+     */
+   public AbstractList()   {
+ 
+   }
+   /**
+    * pre: ninguna<p>
+    * post: retorna true si la lista no tiene elementos.
+    * @return true o false
+    */
+   @Override
+   public boolean isEmpty()
+   {
+      return size() == 0;
+   }
+  /**
+   * pre: value is not null<p>
+   * post: returns true iff list contains an object equal to value.
+   */
+   @Override
+  public boolean contains(E value)  {
+	return -1 != indexOf(value);
+  }
+  
+}
---
+++ src/AbstractStack.java
@@ -0,0 +1,43 @@
+
+/**
+ * Clase abstracta que contiene algunos metodos comunes en las implementaciones de stackXXX y que <p>
+ * implementa a la interfaz iPila.
+ * @author Andrea Elias
+ * Rodrigo Garoz
+ */
+public abstract class AbstractStack<E> implements iPila<E> {
+    
+     /**
+         * pre: el stack no debe estar vacio<p>
+         * post: el item mas recientemente metido al stack se saca primero.
+        */
+    @Override
+    public abstract E pop();
+     /**
+         * pre: el stack no esta vacio
+         * post: el valor del tope a ser sacado es returnado
+         * @return el objeto del tope
+         */
+    @Override
+    public abstract E peek();
+    /**
+    * pre: ninguna<p>
+    * post: retorna true si el stack no tiene elementos.
+    * @return true o false
+    */
+    @Override
+    public boolean empty() {
+        return size() == 0;
+    }
+    /**
+         * post: se retorna el numero de elementos del stack
+         * @return un entero
+         */
+    @Override
+    public int size()    {
+            return size();
+    }
+
+    
+    
+}
